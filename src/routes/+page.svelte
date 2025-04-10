fe2d00b2f11b62d231b1a5b322cd29c28f84ace5
Nils Wrenger <nils@wrenger.net>
2025-04-10 15:58:45 +0200

    :sparkles: Improved window position and preventing now unwanted selections by small delay

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -10,16 +10,37 @@
 	const angleStep = 360 / items.length;
 
 	const size = 2 * (radius + iconSize / 2) + 2 * padding;
-
-	let current_option: string | undefined = $state();
-	let current_window = getCurrentWindow();
+	const current_window = getCurrentWindow();
 
 	current_window.setSize(new LogicalSize(size, size));
-	current_window.listen('select-option', () => {
+	current_window.listen('select-option', async () => {
 		if (current_option) {
-			invoke(current_option);
+			await invoke(current_option);
+			current_option = undefined;
 		}
 	});
+
+	const timeout_duration = 75;
+	let active_timeout: NodeJS.Timeout | undefined = $state();
+	let current_option: string | undefined = $state();
+
+	function mouseouseEnter(itemAction: string) {
+		if (active_timeout) {
+			clearTimeout(active_timeout);
+		}
+		active_timeout = setTimeout(() => {
+			current_option = itemAction;
+			active_timeout = undefined;
+		}, timeout_duration);
+	}
+
+	function mouseouseLeave() {
+		if (active_timeout) {
+			clearTimeout(active_timeout);
+			active_timeout = undefined;
+		}
+		current_option = undefined;
+	}
 </script>
 
 <div class="flex items-center justify-center h-full">
@@ -27,13 +48,14 @@
 		{#each items as item, i}
 			{@const angle = angleStep * i - 90}
 			<button
-				class="absolute btn-icon cursor-pointer {current_option === item.action
-					? 'outline preset-tonal-success'
-					: 'preset-tonal-surface'}"
+				class="absolute btn-icon cursor-pointer transition-all focus:outline-none
+										{current_option === item.action
+					? 'outline preset-tonal-success duration-75'
+					: 'preset-tonal-surface duration-0'}"
 				title={item.descrption}
 				onfocus={() => {}}
-				onmouseover={() => (current_option = item.action)}
-				onmouseleave={() => (current_option = undefined)}
+				onmouseover={() => mouseouseEnter(item.action)}
+				onmouseleave={mouseouseLeave}
 				style={`
 					top: 50%;
 					left: 50%;
