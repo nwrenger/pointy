d7cf93aa9bdeacc907f74daa8580326d02c9c6bc
Nils Wrenger <nils@wrenger.net>
2025-04-14 18:11:30 +0200

    :sparkles: Disabled context menu

--- app/src/routes/+layout.svelte
+++ app/src/routes/+layout.svelte
@@ -2,6 +2,13 @@
 	import '../app.css';
 
 	let { children } = $props();
+
+	// Disable context menu
+	function onContextMenu(e: MouseEvent) {
+		e.preventDefault();
+	}
 </script>
+
+<svelte:document oncontextmenu={onContextMenu} />
 
 {@render children()}
